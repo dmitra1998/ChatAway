5081dbd77d434367353920a1d9c14b6403a1f630
Dhritabrata <dhritabratamitra@gmail.com>
2025-07-17 10:52:04 -0400

    Tried another fix for Render hosting

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,8 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/^\/(?!api).*/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+      res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    } else {
+      next();
+    }
   });
 }
 
